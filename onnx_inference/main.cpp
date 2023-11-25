7fa4f8182a3c4ea093bb039fb04dee0254c331ca
vinceamaz <54095269+vinceamaz@users.noreply.github.com>
2023-11-25 19:49:11 +0800

    enable batch inference

--- onnx_inference/main.cpp
+++ onnx_inference/main.cpp
@@ -51,13 +51,19 @@ Ort::Value vec_to_tensor(std::vector<T>& data, const std::vector<std::int64_t>& 
 int main()
 {
     int device_id = 0;
+    int batch_size = 2;
+
     // onnxruntime 模型路径需要是宽字符 wstring
     std::string model_path = "model/pipeline_xgb.onnx";
     std::wstring w_model_path = std::wstring(model_path.begin(), model_path.end());
 
-    // 读取输入数据
-    std::string arr_path = "input/array_data.bin";
-    std::vector<float> input_tensor_values = readArrayFromFile(arr_path);
+    // Read multiple input arrays
+    std::vector<std::vector<float>> input_arrs;
+    for (int i = 0; i < batch_size; ++i) {
+        std::string arr_path = "input/array_data.bin";
+        std::vector<float> input_arr = readArrayFromFile(arr_path);
+        input_arrs.push_back(input_arr);
+    }
 
     // ----------------------------------------------------------- 
     // 2. onnxruntime 初始化
@@ -69,7 +75,7 @@ int main()
     std::cout << "onnxruntime inference try to use GPU Device: " << device_id << std::endl;
     auto status = OrtSessionOptionsAppendExecutionProvider_CUDA(session_options, device_id);
     // auto status = OrtSessionOptionsAppendExecutionProvider_CPU(session_options, device_id);
-    Ort::Session session_(env, w_model_path.c_str(), session_options);
+    Ort::Session session(env, w_model_path.c_str(), session_options);
 
     // ----------------------------------------------------------- 
     // 3. 从模型中读取输入和输入信息
@@ -79,22 +85,16 @@ int main()
     std::vector<std::string> input_names;
     std::vector<std::int64_t> input_shapes;
     std::cout << "Input Node Name/Shape (" << input_names.size() << "):" << std::endl;
-    for (std::size_t i = 0; i < session_.GetInputCount(); i++) {
-        input_names.emplace_back(session_.GetInputNameAllocated(i, allocator).get());
-        input_shapes = session_.GetInputTypeInfo(i).GetTensorTypeAndShapeInfo().GetShape();
+    for (std::size_t i = 0; i < session.GetInputCount(); i++) {
+        input_names.emplace_back(session.GetInputNameAllocated(i, allocator).get());
+        input_shapes = session.GetInputTypeInfo(i).GetTensorTypeAndShapeInfo().GetShape();
         std::cout << "\t" << input_names.at(i) << " : " << print_shape(input_shapes) << std::endl;
-    }
-    // some models might have negative shape values to indicate dynamic shape, e.g., for variable batch size.
-    for (auto& s : input_shapes) {
-        if (s < 0) {
-            s = 1;
-        }
     }
 
     // print (name/shape) of outputs
     std::vector<std::string> output_names;
-    for (std::size_t i = 0; i < session_.GetOutputCount(); i++) {
-        output_names.emplace_back(session_.GetOutputNameAllocated(i, allocator).get());
+    for (std::size_t i = 0; i < session.GetOutputCount(); i++) {
+        output_names.emplace_back(session.GetOutputNameAllocated(i, allocator).get());
     }
 
     // Assume model has 1 input node and 2 output nodes.
@@ -103,21 +103,25 @@ int main()
     // ----------------------------------------------------------- 
     // 4. 构造输入 tensor
 
-    // Create a single Ort tensor of random numbers
-    auto input_shape = input_shapes;
-    auto total_number_elements = calculate_product(input_shape);
+    auto input_node_dims = input_shapes;
+    assert(input_node_dims[0] == -1);  // symbolic dimensions are represented by a -1 value
+    input_node_dims[0] = batch_size;
+    auto total_number_elements = calculate_product(input_node_dims);
 
-    // generate random numbers in the range [0, 255]
-    // std::vector<float> input_tensor_values(total_number_elements);
-    // std::generate(input_tensor_values.begin(), input_tensor_values.end(), [&] { return rand() % 255; });
+    std::vector<float> input_tensor_values(total_number_elements);
+    // 将 input_arrs 复制到 input_tensor_values
+    for (const auto& input_arr : input_arrs) {
+        input_tensor_values.insert(input_tensor_values.end(), input_arr.begin(), input_arr.end());
+    }
 
     // 将读取的 vector 转成 ort tensor
     std::vector<Ort::Value> input_tensors;
-    input_tensors.emplace_back(vec_to_tensor<float>(input_tensor_values, input_shape));
+    input_tensors.emplace_back(vec_to_tensor<float>(input_tensor_values, input_node_dims));
 
-    // 确保输入 ort tensor 的 shape 与 onnx 模型要求的一样
-    assert(input_tensors[0].IsTensor() && input_tensors[0].GetTensorTypeAndShapeInfo().GetShape() == input_shape);
-    std::cout << "\ninput_tensor shape: " << print_shape(input_tensors[0].GetTensorTypeAndShapeInfo().GetShape()) << std::endl;
+    // double-check the dimensions of the input tensor
+    assert(input_tensors[0].IsTensor() &&
+        input_tensors[0].GetTensorTypeAndShapeInfo().GetShape() == input_node_dims);
+    std::cout << "input_tensor shape: " << print_shape(input_tensors[0].GetTensorTypeAndShapeInfo().GetShape()) << std::endl;
 
     // pass data through model
     std::vector<const char*> input_names_char(input_names.size(), nullptr);
@@ -128,32 +132,45 @@ int main()
     std::transform(std::begin(output_names), std::end(output_names), std::begin(output_names_char),
         [&](const std::string& str) { return str.c_str(); });
 
+    // ----------------------------------------------------------- 
+    // 5. 推理
 
     std::cout << "Running model..." << std::endl;
+
     try {
-        auto output_tensors = session_.Run(Ort::RunOptions{ nullptr }, input_names_char.data(), input_tensors.data(),
-            input_names_char.size(), output_names_char.data(), output_names_char.size());
-        std::cout << "Done!" << std::endl;
+
+        auto output_tensors = session.Run(Ort::RunOptions{ nullptr },
+            input_names_char.data(),
+            input_tensors.data(),
+            input_names_char.size(),
+            output_names_char.data(),
+            output_names_char.size());
+
+        std::cout << "Done inference!" << std::endl;
 
         // double-check the dimensions of the output tensors
         // NOTE: the number of output tensors is equal to the number of output nodes specifed in the Run() call
         assert(output_tensors.size() == output_names.size() && output_tensors[0].IsTensor());
 
         // 遍历读取每一个输出节点, 可以处理 tensor 和 sequence 的情况
-        for (std::size_t i = 0; i < output_names.size(); i++) {
+        for (std::size_t i = 0; i < output_names.size(); ++i) {
             // GetONNXType(): [UNKNOWN, TENSOR, SEQUENCE, MAP, OPAQUE, SPARSETENSOR, OPTIONAL]
             if (output_tensors[i].GetTypeInfo().GetONNXType() == 1) {
                 // 输出是 tensor 的情况
                 const float* output_data_ptr = output_tensors[i].GetTensorMutableData<float>();
-                std::cout << output_names[i] << " is " << *output_data_ptr << std::endl;
+                
+                for (int j = 0; j < batch_size; ++j) {
+
+                    const float* out = output_data_ptr + j;
+                    std::cout << j + 1 << "'th instance in batch (output name: " << output_names[i] << "): " << *out << std::endl;
+                }
             }
             else if (output_tensors[i].GetTypeInfo().GetONNXType() == 2) {
                 // 输出是 sequence 的情况
-                size_t num_values = output_tensors[i].GetCount();
 
-                for (size_t idx = 0; idx < num_values; ++idx) {
+                for (size_t j = 0; j < batch_size; ++j) {
 
-                    Ort::Value seq_out = output_tensors[i].GetValue(static_cast<int>(idx), allocator);
+                    Ort::Value seq_out = output_tensors[i].GetValue(static_cast<int>(j), allocator);
 
                     // 遍历 sequence 里面每一个元素
                     for (size_t val_idx = 0; val_idx < seq_out.GetCount(); ++val_idx) {
@@ -164,13 +181,13 @@ int main()
                             // float
                             Ort::Value float_out = seq_out.GetValue(static_cast<int>(val_idx), allocator);
                             const float* float_result_ptr = float_out.GetTensorMutableData<float>();
-                            std::cout << output_names[i] << "'s float output is " << *float_result_ptr << std::endl;
+                            std::cout << j + 1 << "'th instance in batch (output name: " << output_names[i] << ") float output: " << *float_result_ptr << std::endl;
                         }
                         else if (tensor_dtype > 1 && tensor_dtype < 8) {
                             // int
                             Ort::Value int_out = seq_out.GetValue(static_cast<int>(val_idx), allocator);
                             const int* int_result_ptr = int_out.GetTensorMutableData<int>();
-                            std::cout << output_names[i] << "'s int output is " << *int_result_ptr << std::endl;
+                            std::cout << j + 1 << "'th instance in batch (output name: " << output_names[i] << ") int output: " << *int_result_ptr << std::endl;
                         }
                         else {
                             std::cout << "Encountered new ONNXTensorElementDataType type: " << tensor_dtype << std::endl;
